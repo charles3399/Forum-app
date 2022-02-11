ff1b4b35b375d9dbc63600c4390d632adb0fe98b
charles3399 <bernaldezsay@gmail.com>
2022-02-11 08:04:56 +0800

    added Vue 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+import { createApp } from 'vue'
+
+import Hello from './components/Hello.vue';
+
+const app = createApp({});
+
+app.component('hello', Hello).mount('#app');
---
+++ resources/js/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ greeting }}</h1>
+</template>
+<script>
+export default {
+    name: 'Hello',
+    setup() {
+        const greeting = 'Hello world'
+
+        return {
+            greeting
+        }
+    }
+}
+</script>
